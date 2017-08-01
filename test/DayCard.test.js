c1af25c90e583227d7ae851617c86dd295bbd7f9
Christie Lynam <christielynam@gmail.com>
2017-08-01 14:38:40 -0600

    card testing not working

--- test/DayCard.test.js
+++ test/DayCard.test.js
@@ -8,7 +8,8 @@ describe('DayCard', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<DayCard data={dataMock} />);
+    let day = dataMock.forecast.simpleforecast[0]
+    wrapper = shallow(<DayCard img={day.icon} high={day.high.fahrenheit} low={day.low.fahrenheit} day={day.date.weekday} key={day.period} />);
   });
 
   afterEach(() => {
@@ -20,26 +21,23 @@ describe('DayCard', () => {
   });
 
   it('should display a condition icon', () => {
-    const component = shallow(<HourCard />);
-    const icon = component.find('img');
-    expect(image.prop('src')).toEqual('http://icons.wxug.com/i/c/k/clear.gif');
+    const icon = wrapper.instance().find('img');
+
+    expect(icon.prop('src')).toEqual('http://icons.wxug.com/i/c/k/clear.gif');
   });
 
   it('should display a high temp', () => {
-    const component = shallow(<HourCard />);
-    const hiTemp = component.find('p.dayHigh');
-    expect(hiTemp.text()).toEqual('98');
+    const hiTemp = wrapper.instance().find('p.dayHigh');
+    expect(hiTemp.text()).toEqual('H: 98');
   });
 
   it('should display a low temp', () => {
-    const component = shallow(<HourCard />);
-    const lowTemp = component.find('p.dayLow');
-    expect(lowTemp.text()).toEqual('70');
+    const lowTemp = wrapper.instance().find('p.dayLow');
+    expect(lowTemp.text()).toEqual('L: 70');
   });
 
   it('should display a day', () => {
-    const component = shallow(<HourCard />);
-    const day = component.find('p.day');
+    const day = wrapper.instance().find('p.day');
     expect(day.text()).toEqual('Monday');
   });
 });
