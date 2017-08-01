132098d4cc5d92a849d75ffa3a6bb072cce1e8f0
Christie Lynam <christielynam@gmail.com>
2017-08-01 14:16:05 -0600

    more weather card testing

--- test/DayCard.test.js
+++ test/DayCard.test.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import DayCard from '../lib/Components/DayCard/DayCard';
 import localStorageMock from '../__mock__/localStorageMock.js';
+import { dataMock } from '../__mock__/dataMock.js';
 
 describe('DayCard', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<DayCard />);
+    wrapper = shallow(<DayCard data={dataMock} />);
   });
 
   afterEach(() => {
@@ -21,24 +22,24 @@ describe('DayCard', () => {
   it('should display a condition icon', () => {
     const component = shallow(<HourCard />);
     const icon = component.find('img');
-    expect(image.prop('src')).toEqual(`assets/${hour.img}.svg`);
+    expect(image.prop('src')).toEqual('http://icons.wxug.com/i/c/k/clear.gif');
   });
 
   it('should display a high temp', () => {
     const component = shallow(<HourCard />);
     const hiTemp = component.find('p.dayHigh');
-    expect(hiTemp.text()).toEqual('');
+    expect(hiTemp.text()).toEqual('98');
   });
 
   it('should display a low temp', () => {
     const component = shallow(<HourCard />);
     const lowTemp = component.find('p.dayLow');
-    expect(lowTemp.text()).toEqual('');
+    expect(lowTemp.text()).toEqual('70');
   });
 
   it('should display a day', () => {
     const component = shallow(<HourCard />);
     const day = component.find('p.day');
-    expect(day.text()).toEqual('');
+    expect(day.text()).toEqual('Monday');
   });
 });
--- test/HourCard.test.js
+++ test/HourCard.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import HourCard from '../lib/Components/HourCard/HourCard';
 import localStorageMock from '../__mock__/localStorageMock.js';
+import { dataMock } from '../__mock__/dataMock.js';
 
 describe('HourCard', () => {
   let wrapper;
@@ -21,13 +22,13 @@ describe('HourCard', () => {
   it('should display a condition icon', () => {
     const component = shallow(<HourCard />);
     const icon = component.find('img');
-    expect(image.prop('src')).toEqual(`assets/${hour.img}.svg`);
+    expect(image.prop('src')).toEqual('http://icons.wxug.com/i/c/k/clear.gif');
   });
 
   it('should display a temp', () => {
     const component = shallow(<HourCard />);
     const temp = component.find('h3');
-    expect(temp.text()).toEqual('');
+    expect(temp.text()).toEqual('86.1');
   });
 
   it('should display a time', () => {
