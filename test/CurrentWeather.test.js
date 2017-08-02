9985b5d9f029210134e7fb4d8dd39c46a5e7659c
Jack Mallahan <jackmallahan@gmail.com>
2017-08-01 21:53:42 -0600

    finished current weather tests

--- test/CurrentWeather.test.js
+++ test/CurrentWeather.test.js
@@ -9,7 +9,6 @@ import Location from '../lib/Components/Location'
 describe('CurrentWeather', () => {
   let wrapper;
   let location = new Location(weatherData)
-  console.log();
 
   beforeEach(() => {
     wrapper = shallow(<CurrentWeather weather={location} />);
@@ -19,13 +18,47 @@ describe('CurrentWeather', () => {
     localStorage.clear();
   });
 
-  it.only('should exist', () => {
+  it('should exist', () => {
     expect(wrapper).toBeDefined();
   });
 
   it('should display the saved city', () => {
-    // expect(wrapper.props().children.find(h3.city)).toEqual('Birmingham, AL')
+    expect(location.location).toEqual('Birmingham, AL')
   })
 
+  it('should display the day', () => {
+    expect(location.day).toEqual('Tuesday')
+  })
 
+  it('should display the month', () => {
+    expect(location.month).toEqual('August')
+  })
+
+  it('should display the date', () => {
+    expect(location.date).toEqual(1)
+  })
+
+  it('should display the current temperature', () => {
+    expect(location.currentTemp).toEqual(88)
+  })
+
+  it('should display the current condition', () => {
+    expect(location.condition).toEqual('Partly Cloudy')
+  })
+
+  it('should display an icon that represents the condition', () => {
+    expect(location.icon).toEqual('mostlycloudy')
+  })
+
+  it("should display today's expected high", () => {
+    expect(weatherData.forecast.simpleforecast.forecastday[0].high.fahrenheit).toEqual(88)
+  })
+
+  it("should display today's expected low", () => {
+    expect(weatherData.forecast.simpleforecast.forecastday[0].low.fahrenheit).toEqual(72)
+  })
+
+  it('should display a daily report', () => {
+    expect(location.dailyReport).toEqual('Mainly cloudy. Lows overnight in the low 70s')
+  })c
 })
